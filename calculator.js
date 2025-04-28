f676ab3493606277a4968a68445aac636efca8bd
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-28 09:03:03 +0000

    Standard boilerplate and css layout

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// const row = 5;
+// const col=4;
+
+
+// function buttonCreator(row, col) {
+//   const buttonBox = document.querySelector(".buttonBox");
+//   for (let i=0; i < row; i++) {
+//     let row = document.createElement("div");
+//     row.setAttribute("class", "buttonRow");
+//     for (let j=0; j < col; j++) {
+//       let col = document.createElement("div");
+//       col.setAttribute("class", "button");
+//       row.appendChild(col);
+//     }
+//     buttonBox.appendChild(row);
+//   }
+// }
