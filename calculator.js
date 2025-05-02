6e63b03f32ba4d2e74781f5c76c5fa4ed467d70c
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-05-02 10:51:42 +0000

    Added Placeholder function for button display

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let firstNumber = 1;
 let operator = "*";
 let secondNumber = 2;
+let calcDisplay = document.querySelector(".calcDisplay");
 
 function operate(firstNumber, operator, secondNumber) {
   switch(operator) {
@@ -15,4 +16,11 @@ function operate(firstNumber, operator, secondNumber) {
   }
 }
 
+function buttonDisplay() {
+  let digit = document.querySelectorAll(".digit");
+  digit.addEventListener("click", () => {
+    //Fill first number and second number accordingly and display them.
+    return;
+  })
+}
 console.log(operate(firstNumber,operator,secondNumber));
