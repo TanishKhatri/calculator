36a400c306299657b762491069b15599ccc5bc38
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-05-02 10:39:45 +0000

    Added a function which can do calculations

--- calculator.js
+++ calculator.js
@@ -1,17 +1,18 @@
-// const row = 5;
-// const col=4;
+let firstNumber = 1;
+let operator = "*";
+let secondNumber = 2;
 
+function operate(firstNumber, operator, secondNumber) {
+  switch(operator) {
+    case "+":
+      return firstNumber + secondNumber;
+    case "-":
+      return firstNumber - secondNumber;
+    case "/":
+      return firstNumber/secondNumber;
+    case "*":
+      return firstNumber*secondNumber;    
+  }
+}
 
-// function buttonCreator(row, col) {
-//   const buttonBox = document.querySelector(".buttonBox");
-//   for (let i=0; i < row; i++) {
-//     let row = document.createElement("div");
-//     row.setAttribute("class", "buttonRow");
-//     for (let j=0; j < col; j++) {
-//       let col = document.createElement("div");
-//       col.setAttribute("class", "button");
-//       row.appendChild(col);
-//     }
-//     buttonBox.appendChild(row);
-//   }
-// }
+console.log(operate(firstNumber,operator,secondNumber));
