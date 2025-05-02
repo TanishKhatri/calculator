c42de06e8ee0b5c48544ed6b9baea623998aea30
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-05-02 15:23:00 +0000

    The calculator now displays numbers on click

--- calculator.js
+++ calculator.js
@@ -1,26 +1,38 @@
-let firstNumber = 1;
+let firstNumber = "";
 let operator = "*";
-let secondNumber = 2;
-let calcDisplay = document.querySelector(".calcDisplay");
+let secondNumber = "";
+
+//While true change first number else change second number
+let fnOrSn = true;
+const calcDisplay = document.querySelector(".calcDisplay");
+const calcPreview = document.querySelector(".calcPreview");
+
 
 function operate(firstNumber, operator, secondNumber) {
+  let fn = parseInt(firstNumber);
+  let sn = parseInt(secondNumber);
   switch(operator) {
     case "+":
-      return firstNumber + secondNumber;
+      return fn + sn;
     case "-":
-      return firstNumber - secondNumber;
+      return fn - sn;
     case "/":
-      return firstNumber/secondNumber;
+      return fn/sn;
     case "*":
-      return firstNumber*secondNumber;    
+      return fn*sn;    
   }
 }
 
-function buttonDisplay() {
-  let digit = document.querySelectorAll(".digit");
-  digit.addEventListener("click", () => {
-    //Fill first number and second number accordingly and display them.
-    return;
-  })
+//First do first number once operator is chosen do second number
+function numberDisplay() {
+  let digits = document.querySelectorAll(".digit");
+  let currentNumber = "";
+  digits.forEach((digit) => {
+    digit.addEventListener("click", (e) => {
+      currentNumber = e.target.textContent;
+      calcDisplay.textContent === "0" ? calcDisplay.textContent = currentNumber : calcDisplay.textContent += currentNumber;
+    });
+  });
 }
-console.log(operate(firstNumber,operator,secondNumber));
+
+numberDisplay();
