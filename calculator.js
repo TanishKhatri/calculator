66c513ade3bf583bf22d7ef7bda52795d0868153
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-05-15 06:34:42 +0000

    Basic Operations and Equals now Work!
    Rewrote most of the code to make the basic functions work and
    tweaked css to do click darken by default.

--- calculator.js
+++ calculator.js
@@ -1,101 +1,130 @@
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
+let replaceFlag = true;
+let fnOrSn = true;
 
-//While true change first number else change second number
-let operatorTrigger = false;
 const calcDisplay = document.querySelector(".calcDisplay");
 const calcPreview = document.querySelector(".calcPreview");
 
-
 function operate(firstNumber, operator, secondNumber) {
-  let fn = parseInt(firstNumber);
-  let sn = parseInt(secondNumber);
-  switch(operator) {
+  let fn = parseFloat(firstNumber);
+  let sn = parseFloat(secondNumber);
+  switch(operator){
     case "+":
       return fn + sn;
     case "-":
       return fn - sn;
     case "/":
-      return fn/sn;
+      if (sn === 0) {
+        return "N/0=Bad!";
+      } else {
+        return fn/sn;
+      }
     case "*":
-      return fn*sn;    
+      return fn*sn;
+    default:
+      console.log("Error: Operate was parsed wrong operator or numbers");
+      break;  
   }
 }
 
-function numberDisplay() {
-  let digits = document.querySelectorAll(".digit");
-  let currentNumber = "";
-  digits.forEach((digit) => {
-    digit.addEventListener("click", (e) => {
-      if (calcDisplay.textContent.length < 10) {
-        if (!operatorTrigger) {
-          currentNumber = e.target.textContent;
-          calcDisplay.textContent === "0" ? calcDisplay.textContent = currentNumber : calcDisplay.textContent += currentNumber;
-          calcPreview.textContent += currentNumber;
-          firstNumber += currentNumber;
-        } else {
-          currentNumber = e.target.textContent;
-          calcDisplay.textContent === "0" ? calcDisplay.textContent = currentNumber : calcDisplay.textContent += currentNumber;
-          calcPreview.textContent += currentNumber;
-          secondNumber += currentNumber;
+function numberClickEvent() {
+  //List of all buttons
+  let btnList = document.querySelectorAll(".digit");
+
+  function fillNumberVariables(numberClicked) {
+    if (fnOrSn) {
+      if (replaceFlag) {
+        firstNumber = numberClicked;
+      } else {
+        firstNumber += numberClicked;
+      }
+    } else {
+      if (replaceFlag) {
+        secondNumber = numberClicked;
+      } else {
+        secondNumber += numberClicked;
+      }
+    }
+  }
+
+  //Run the provided function for each button
+  btnList.forEach((button) => {
+    //Add an event listener which runs the provided function when button is clicked.
+    button.addEventListener("click", (e) => {
+      //Replace the text content with the pressed number if replaceFlag = true, else append to it.
+      let numberClicked = e.target.textContent
+      if (replaceFlag) {
+        calcDisplay.textContent = numberClicked;
+        fillNumberVariables(numberClicked);
+        replaceFlag = false;
+      } else {
+        //Make sure that the text doesn't overflow.
+        if (calcDisplay.textContent.length <= 10) {
+          calcDisplay.textContent += numberClicked;
+          fillNumberVariables(numberClicked);
         }
       }
-    });
-  });
-}
-
-function operatorButton () {
-  let operators = document.querySelectorAll(".std");
-  operators.forEach((op) => {
-    op.addEventListener("click", (e) => {
-      switch(e.target.id) {
-        case "add":
-          operator = "+";
-          calcPreview.textContent = `${firstNumber + " " + operator + " "}`;
-          operatorTrigger = true;
-          break;
-        case "divide":
-          operator = "/";
-          calcPreview.textContent = `${firstNumber + " " + operator + " "}`;
-          operatorTrigger = true;
-          break;
-        case "multiply":
-          operator = "*";
-          calcPreview.textContent = `${firstNumber + " " + operator + " "}`;
-          operatorTrigger = true;
-          break;
-        case "subtract":
-          operator = "-"
-          calcPreview.textContent = `${firstNumber + " " + operator + " "}`;
-          operatorTrigger = true;
-          break;      
-      }
-    });
-  });
-}
-
-function addClearButton () {
-  let clearButton = document.querySelector("#clearButton");
-  clearButton.addEventListener("click", () => {
-    calcDisplay.textContent = "0";
-    calcPreview.textContent = "";
-    firstNumber = "";
-    secondNumber= "";
-    operator = "";
-  });
-}
-
-function changeButtonColorOnClick () {
-  let btn = document.querySelectorAll(".button");
-  btn.forEach((button) => {
-    button.addEventListener("mousedown", (e) => {e.target.style.backgroundColor = "#121212"});
-    button.addEventListener("mouseup", (e) => {e.target.style.backgroundColor = ""});
-    button.addEventListener("mouseleave", (e) => {e.target.style.backgroundColor = ""});
+    })
   })
 }
 
+function operatorButton() {
+  let operatorList = document.querySelectorAll(".std");
+  operatorList.forEach((op) => {
+    op.addEventListener("click", (e) => {
+      switch (e.target.id) {
+        case "add":
+          operator = "+";
+          fnOrSn = false;
+          replaceFlag = true;
+          break;
+        case "subtract":
+          operator = "-";
+          fnOrSn = false;
+          replaceFlag = true;
+          break;
+        case "multiply":
+          operator = "*";
+          fnOrSn = false;
+          replaceFlag = true;
+          break;
+        case "divide":
+          operator = "/";
+          fnOrSn = false;
+          replaceFlag = true;
+          break;  
+      }
+    });
+  });
+}
+
+function equalButton() {
+  let eq = document.querySelector(".equal");
+  eq.addEventListener("click", () => {
+    let result = operate(firstNumber, operator, secondNumber);
+    calcDisplay.textContent = result;
+    firstNumber = `${result}`;
+    replaceFlag = true;
+    fnOrSn = true;
+  })
+}
+
+function allClearButton() {
+  let clearButton = document.querySelector("#clearButton");
+  clearButton.addEventListener("click", () => {
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
+    calcDisplay.textContent = "0";
+    calcPreview.textContent = "";
+    replaceFlag = true;
+    fnOrSn = true;
+  })
+}
+
+numberClickEvent();
+allClearButton();
 operatorButton();
-changeButtonColorOnClick();
-addClearButton();
-numberDisplay();
+equalButton();
